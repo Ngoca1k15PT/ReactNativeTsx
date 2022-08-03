63c3771c07865d0efb9969ed1e93a684e1371d49
Ngoc <ngocdh@flydinotech.com>
2022-08-03 16:37:45 +0700

    updatev1

--- app/components/navigation/TabBarNavigation.tsx
+++ app/components/navigation/TabBarNavigation.tsx
@@ -4,11 +4,21 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Exchange from '../exchange/Exchange';
 import Setting from '../setting/Setting';
 import Home from '../home/Home';
+import Constant from '../../controller/Constant';
 
 type RootStackParamList = {
-  Home: undefined;
+  Home: {
+    id: string;
+  };
+  Detail: {
+    productId: number;
+  };
   Exchange: undefined;
   Setting: {userID: String};
 };
 
 const RootStack = createStackNavigator<RootStackParamList>();
+
+<RootStack.Navigator initialRouteName={'Home'}>
+  <RootStack.Screen name={'Home'} component={Home} />
+</RootStack.Navigator>;
