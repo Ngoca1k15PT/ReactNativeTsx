18bd40f1302e5a85d36d5b114abadab8b5b1e295
Ngoc <ngocdh@flydinotech.com>
2022-09-22 15:12:20 +0700

    Update Navigation

---
+++ app/components/navigation/DrawerNavigation.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from '../home/Home';
+import RootStackParamList from './RootStackParam';
+
+const Drawer = createDrawerNavigator();
